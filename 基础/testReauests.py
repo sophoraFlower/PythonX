3500c9e1e49b6afe502cf58596e6393f3ffefbdf
sophoraFlower <2268228773@qq.com>
2018-02-06 12:22:12 +0800

    update ..

--- 基础/testReauests.py
+++ 基础/testReauests.py
@@ -1,27 +1,15 @@
 import requests
-from bs4 import BeautifulSoup as bs
+
+response = requests.get('https://www.zhanqi.tv/api/actives/dbqb/rank')
+content = response.json()
+cornNums = content['data']
+cornNumAll = 0
+for i in range(30):
+    cornNumAll += int(cornNums[0]['cornNum'])
+
+print(cornNumAll)
 
 
-response_data = requests.get('http://python123.io/ws/demo.html')
-html_data = response_data.text
 
-soup = bs(html_data, 'lxml')
-print(soup.prettify())
 
-# tag1 = soup.body.contents
-# tag2 = soup.body.children
-# tag3 = soup.body.descendants
-
-# print(tag1)
-# for tag in tag2:
-#     print(tag)
-
-# for tag in tag3:
-#     print(tag)
-
-tag4 = soup.p
-tag5 = tag4.next_siblings
-print(tag4)
-for tag in tag5:
-    print(tag)
 
