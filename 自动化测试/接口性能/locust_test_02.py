a7fe5e2618d5e72c6cf89ee879baabe10151b281
sophoraFlower <houle.cf@foxmail.com>
2018-11-28 18:00:54 +0800

    11/28 update about
    Signed-off-by: sophoraFlower <houle.cf@foxmail.com>

---
+++ 自动化测试/接口性能/locust_test_02.py
@@ -0,0 +1,16 @@
+# coding:utf-8
+from locust import HttpLocust, TaskSet, task
+
+
+class UserBehavior(TaskSet):
+
+    @task()
+    def baidu_page(self):
+        self.client.get("/")
+
+
+class WebsiteUser(HttpLocust):
+    task_set = UserBehavior
+    min_wait = 5000
+    max_wait = 9000
+
