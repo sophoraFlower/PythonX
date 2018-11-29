655631d6bd14f46b604a1ce85f926a51d6231768
sophoraFlower <2268228773@qq.com>
2018-11-29 23:16:19 +0800

    update flask_web 11/29

--- Python应用/Flask_web/app/home/views.py
+++ Python应用/Flask_web/app/home/views.py
@@ -1,7 +1,18 @@
 # coding:utf-8
 from . import home
+from flask import render_template, redirect, url_for
 
 
 @home.route("/")  # 配置home的特定路由
 def index():
-    return "<h1 style='color:green'>this is home</h1>"
+    return render_template("home/index.html")
+
+
+@home.route("/login/")
+def login():
+    return render_template("home/login.html")
+
+
+@home.route("/logout/")
+def logout():
+    return redirect(url_for("home.login"))
