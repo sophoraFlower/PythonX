0eca011c4c76aead7fe16faa89a8b7f42522d64f
sophoraFlower <2268228773@qq.com>
2018-12-17 01:00:38 +0800

    update Flask_web project

--- Python应用/Flask_web/app/home/views.py
+++ Python应用/Flask_web/app/home/views.py
@@ -16,3 +16,49 @@ def login():
 @home.route("/logout/")
 def logout():
     return redirect(url_for("home.login"))
+
+
+@home.route("/register/")
+def register():
+    return render_template("home/register.html")
+
+
+# 会员中心页面
+@home.route("/user/")
+def user():
+    return render_template("home/user.html")
+
+
+@home.route("/pwd/")
+def pwd():
+    return render_template("home/pwd.html")
+
+
+@home.route("/comments/")
+def comments():
+    return render_template("home/comments.html")
+
+
+@home.route("/loginlog/")
+def loginlog():
+    return render_template("home/loginlog.html")
+
+
+@home.route("/moviecol/")
+def moviecol():
+    return render_template("home/moviecol.html")
+
+
+@home.route("/animation/")
+def animation():
+    return render_template("home/animation.html")
+
+
+@home.route("/search/")
+def search():
+    return render_template("home/search.html")
+
+
+@home.route("/play/")
+def play():
+    return render_template("home/play.html")
