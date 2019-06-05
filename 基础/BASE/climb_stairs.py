50f237339d5283d8ab9338ffe73d1a3733efeaa5
sophoraFlower <2268228773@qq.com>
2019-06-05 20:42:35 +0800

    update..0605

--- 基础/BASE/climb_stairs.py
+++ 基础/BASE/climb_stairs.py
@@ -1,5 +1,6 @@
 import math
 import time
+import random
 
 
 # n >= 1 楼梯长度
@@ -11,9 +12,20 @@ def min_climb_stairs(n):
         return min_climb_stairs(n-2) + min_climb_stairs(1)
 
 
-def yinghe_climb_stairs(n):
-    # 可走一步或两步,求最小步数
-    ax = 0  # 走一步的数量
+# n >= 1 楼梯长度
+def climb_stairs(n):
+    # 可走一步或两步,求路径数
+    if n == 1:
+        return 1
+    elif n == 2:
+        return 2
+    else:
+        return climb_stairs(n-2) + climb_stairs(n-1)
+
+
+def min_yinghe_climb_stairs(n):
+    # 可走一步或两步,求路径数
+    ax = 1  # 走一步的数量
     ways = []
     if n == 1:
         return 1
@@ -25,12 +37,27 @@ def yinghe_climb_stairs(n):
         return min(ways)
 
 
+def yinghe_climb_stairs(n):
+    # 爬台阶，可走一步或两步,求路径数
+    ax = 1  # 走一步的数量
+    ways = [1*random.randint(0, 2)+2*random.randint(0, 2)]
+    # if n == 1:
+    #     return 1
+    # else:
+    #     while ax <= n:
+    #         by = (n-ax)/2
+    #         ways.append(math.ceil(ax+by))
+    #         ax += 1
+    #     return min(ways)
+    return ways
+
+
 if __name__ == '__main__':
     start1_time = time.perf_counter()
-    print(min_climb_stairs(1001))
+    print(climb_stairs(5))
     end1_time = time.perf_counter()
     print(end1_time-start1_time)
     start2_time = time.perf_counter()
-    print(yinghe_climb_stairs(1001))
+    print(yinghe_climb_stairs(4))
     end2_time = time.perf_counter()
     print(end2_time - start2_time)
