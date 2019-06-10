7587533060a4bd72bb9606ecc6cacd25dbe54bbf
sophoraFlower <2268228773@qq.com>
2019-06-11 03:36:02 +0800

    update..0611

--- Python算法实践/刷题/LinkedList/linked_list_study.py
+++ Python算法实践/刷题/LinkedList/linked_list_study.py
@@ -5,7 +5,7 @@ class Node:
 
 
 head = None
-for count in range(10):
+for count in range(3):
     head = Node(count, head)
 # while head is not None:
 #     print(head.data)
@@ -21,19 +21,80 @@ print(probe.data)
 """
 
 
-def reverse_linked_list(linked_list):
-    if linked_list is None or linked_list.next is None:
-        return
-    probe = None
-    tmp = linked_list.next
-    while tmp:
+# 迭代
+def reverse_linked_list1(linked_list):
+    cur, prev = linked_list, None
+    while cur:
+        cur.next, prev, cur = prev, cur, cur.next
+    return prev
+
+
+# 递归
+def reverse_linked_list2(linked_list):
+    if not linked_list or not linked_list.next:
+        return linked_list
+    else:
+        reverse_linked_list = reverse_linked_list2(linked_list.next)
+        linked_list.next.next = linked_list
+        linked_list.next = None
+        return reverse_linked_list
+
+
+# 常规
+def reverse_linked_list3(linked_list):
+    pre = linked_list
+    cur = linked_list.next
+    pre.next = None
+    while cur:
+        temp = cur.next
+        cur.next = pre
+        pre = cur
+        cur = temp
+    return pre
+
+
+# 常规2
+def reverse_linked_list4(linked_list):
+    # 将原链表的head节点（第一个节点）变成新链表的最后一个节点
+    prev = linked_list
+    # 当前链表相当于原链表删除第一个节点
+    cur = linked_list.next
+    # 新链表的tail节点指向None
+    prev.next = None
+    while cur:
+        # 将cur的下一个节点保存在temp中，也就是第3个节点，因为翻转后，节点2的下一个节点变成了节点1，原先节点2和节点3之间的连接断开，通过节点2就找不到节点3了，因此需要保存
+        temp = cur.next
+        # 将当前节点的下一个节点指向新链表的第一个节点
+        cur.next = prev
+        # 新链表head节点的值为原链表当前节点的值
+        prev = cur
+        # 原链表的当前节点为保存的temp
+        cur = temp
+    return prev
+
+
+# reverse_linked_list_ = reverse_linked_list4(head)
+# while reverse_linked_list_ is not None:
+#     print(reverse_linked_list_.data)
+#     reverse_linked_list_ = reverse_linked_list_.next
+
+
+def swap_pairs(linked_list):
+    if not linked_list or not linked_list.next:
+        return linked_list
+    elif not linked_list.next.next:
+        temp = linked_list
         linked_list = linked_list.next
-        tmp.next = probe
-        tmp = tmp.next
-    return tmp
+        linked_list.next = temp
+        temp.next = None
+        return linked_list
+    else:
+        cur_list = linked_list.next.next
+        new_list = swap_pairs(cur_list)
+        return new_list
 
 
-reverse_linked_list(head)
-while head is not None:
-    print(head.data)
-    head = head.next
+swap_linked_list = swap_pairs(head)
+while swap_linked_list is not None:
+    print(swap_linked_list.data)
+    swap_linked_list = swap_linked_list.next
--- Python算法实践/基础/BASE/0608.py
+++ Python算法实践/基础/BASE/0608.py
@@ -1,11 +1,53 @@
+graph = dict()
+graph["start"] = {}
+graph["start"]["a"] = 6
+graph["start"]["a"] = 2
+
+graph["a"] = {}
+graph["a"]["fin"] = 1
+
+graph["b"] = {}
+graph["b"]["a"] = 3
+graph["b"]["fin"] = 5
+
+graph["fin"] = {}
+
+print(graph)
+
+infinity = float("inf")
+costs = dict()
+costs["a"] = 6
+costs["b"] = 2
+costs["fin"] = infinity
+
+parents = dict()
+parents["a"] = "start"
+parents["b"] = "end"
+parents["fin"] = None
+
+processed = []
 
 
-def nums(list_):
-    if len(list_) == 0:
-        return 0
-    else:
-        return 1 + nums(list_[1:])
+def find_lowest_cost_node(costs_):
+    lowest_cost = float("inf")
+    lowest_cost_node = None
+    for node_ in costs_:
+        cost_ = costs_[node_]
+        if cost_ < lowest_cost and node_ not in processed:
+            lowest_cost = cost_
+            lowest_cost_node = node_
+    return lowest_cost_node
 
 
-print(nums([1, 'mmm', 88, 'kko']))
-
+node = find_lowest_cost_node(costs)
+while node is not None:
+    cost = costs[node]
+    neighbors = graph[node]
+    for n in neighbors.keys():
+        new_cost = cost + neighbors[n]
+        if costs[n] > new_cost:
+            costs[n] = new_cost
+            parents[n] = node
+    processed.append(node)
+    node = find_lowest_cost_node(costs)
+    print(costs)
