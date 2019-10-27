03605f2fd6d0e4e1f7f8ebde11a55d54bb160d94
houlex <houle.cf@foxmail.com>
2019-10-27 16:45:47 +0800

    Base Python study

--- 基础/BASE/PythonBase03/base102201.py
+++ 基础/BASE/PythonBase03/base102201.py
@@ -1,6 +1,5 @@
-
-'''
-
+import copy
+"""
 a = [1]
 b = [1]
 print(a == b)    # True
@@ -13,6 +12,32 @@ print(t1 is t2)    # False
 
 t1[-1].append(5)
 print(t1 == t2)    # False
-'''
+"""
+"""
+l1 = [[1, 2], (30, 40)]
+# 深度拷贝
+l2 = copy.deepcopy(l1)
+l1.append(100)
+l1[0].append(3)
 
+print(l1)  # [[1, 2, 3], (30, 40), 100]
+print(l2)  # [[1, 2], (30, 40)]
 
+l3 = copy.copy(l1)
+l3[1] += (50,)
+print(l1)  # [[1, 2, 3], (30, 40), 100]
+print(l3)  # [[1, 2, 3], (30, 40, 50), 100]
+l3[0].append(4)
+print(l1)  # [[1, 2, 3, 4], (30, 40), 100]
+print(l2)  # [[1, 2], (30, 40)]
+print(l3)  # [[1, 2, 3, 4], (30, 40, 50), 100]
+"""
+
+x = [1]
+x.append(x)
+print(x)  # [1, [...]]
+x = copy.copy(x)
+print(x)
+
+y = copy.deepcopy(x)
+print(y)  # [1]
