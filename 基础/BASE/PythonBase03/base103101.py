9c9d4783717199bb2f8fab7841ab25dbbbd2d80a
houlex <houle.cf@foxmail.com>
2019-11-01 18:56:19 +0800

    up up day day

--- 基础/BASE/PythonBase03/base103101.py
+++ 基础/BASE/PythonBase03/base103101.py
@@ -1,4 +1,5 @@
 import functools
+
 """
 def my_decorator(func):
     # 函数 my_decorator() 就是一个装饰器，它把真正需要执行的函数 greet() 包裹在其中
@@ -49,7 +50,7 @@ def now():
     print('2019-10-31')
 """
 
-
+"""
 def log(text):
     def decorator(func):
         @functools.wraps(func)
@@ -66,5 +67,45 @@ def now():
 
 
 now()
+"""
+
+"""
+def log(level):
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            if level == 'warn':
+                print('print warn log!')
+            else:
+                print('print other log!')
+            return func(*args, **kwargs)
+        return wrapper
+    return decorator
 
 
+@log(level='warn')
+def foo(name, age=None, height=None):
+    print('i am foo' + name)
+    print('my age is %s, height %s' % (age, height))
+
+
+# foo = log(foo)
+foo('biubiubiu~~', 26, 172)
+"""
+
+
+class Foo(object):
+    def __init__(self, func):
+        self._func = func
+
+    def __call__(self, *args, **kwargs):
+        print('class decorator running')
+        self._func()
+        print('class decorator ending')
+
+
+@Foo
+def bar():
+    print('bar')
+
+
+bar()
