a9ceb4553725e7c5dd6996033e144a6ef26da387
houlex <houle.cf@foxmail.com>
2019-11-12 19:35:52 +0800

    up up day day

--- 基础/BASE/PythonBase03/base110201.py
+++ 基础/BASE/PythonBase03/base110201.py
@@ -1,4 +1,18 @@
-# python 基础查漏补全
-# python 进阶课程
-# python web 基础项目
-# web基础之javascript
+
+
+def nth_power(exponent):
+    def exponent_of(base):
+        return base ** exponent
+    return exponent_of  # 返回值是exponent_of函数
+
+
+square = nth_power(2)  # 计算一个数的平方
+cube = nth_power(3)  # 计算一个数的立方
+# 输出
+print(square)
+# 输出
+print(cube)
+
+print(square(2))  # 计算2的平方
+print(cube(2))  # 计算2的立方
+
