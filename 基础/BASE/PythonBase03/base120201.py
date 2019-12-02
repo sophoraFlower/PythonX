bb5dcb2aae32e10b5f7eec3c4661152bb7fed4cc
houlex <houle.cf@foxmail.com>
2019-12-02 18:08:26 +0800

    up up day day

---
+++ 基础/BASE/PythonBase03/base120201.py
@@ -0,0 +1,10 @@
+d1 = {'name': 'jason', 'age': 20, 'gender': 'male'}
+d2 = dict({'name': 'jason', 'age': 20, 'gender': 'male'})
+d3 = dict([('name', 'jason'), ('age', 20), ('gender', 'male')])
+d4 = dict(name='jason', age=20, gender='male')
+print(d1 == d2 == d3 == d4)
+
+s1 = {1, 2, 3}
+s2 = set([1, 2, 3])
+print(s1 == s2)
+
