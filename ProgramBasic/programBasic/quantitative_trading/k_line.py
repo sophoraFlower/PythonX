19343af0efd8131a69f38c9a40fca121a27aeb13
Houle <houle.cf@foxmail.com>
2022-03-28 21:47:25 +0800

    python programing practice 2022/03/28

---
+++ ProgramBasic/programBasic/quantitative_trading/k_line.py
@@ -0,0 +1,27 @@
+import os
+import pandas as pd
+
+
+def assert_msg(condition, msg):
+    if not condition:
+        raise Exception(msg)
+
+
+def read_file(filename):
+    # 获得文件绝对路径
+    filepath = os.path.join(os.path.dirname(__file__), filename)
+
+    # 判定文件是否存在
+    assert_msg(os.path.exists(filepath), "文件不存在")
+
+    # 读取CSV文件并返回
+    return pd.read_csv(filepath,
+                       index_col=0,
+                       parse_dates=True,
+                       infer_datetime_format=True)
+
+
+BTCUSD = read_file('../BTCUSD_GEMINI.scv')
+assert_msg(BTCUSD.__len__() > 0, '读取失败')
+print(BTCUSD.head())
+
