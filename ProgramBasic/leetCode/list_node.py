2a9eafae28c6f16697c111b72ddb72c6fa065d65
Houle <houle.cf@foxmail.com>
2022-03-31 17:31:11 +0800

    LeetCode Programming 2022/03/31

--- ProgramBasic/leetCode/list_node.py
+++ ProgramBasic/leetCode/list_node.py
@@ -47,12 +47,60 @@ def print_linked_list(ll: ListNode):
         ll = ll.next
 
 
+def print_linked_list_string(ll: ListNode):
+    string_list = []
+    while ll:
+        string_list.append(ll.val)
+        ll = ll.next
+    print(sorted(string_list))
+
+
+# 通过链表计算 9999+123456=133455
+def addTwoNumbers(l1: ListNode, l2: ListNode) -> ListNode:
+    if l1 is None:
+        return l2
+    if l2 is None:
+        return l1
+    # 创建头节点
+    result = ListNode(0)
+    p = result
+    carry = 0
+    while l1 and l2:
+        p.next = ListNode((l1.val+l2.val+carry) % 10)
+        carry = (l1.val+l2.val+carry) // 10
+        l1 = l1.next
+        l2 = l2.next
+        p = p.next
+    if l1:
+        while l1:
+            p.next = ListNode((l1.val + carry) % 10)
+            carry = (l1.val + carry) // 10
+            l1 = l1.next
+            p = p.next
+    if l2:
+        while l2:
+            p.next = ListNode((l2.val + carry) % 10)
+            carry = (l2.val + carry) // 10
+            l2 = l2.next
+            p = p.next
+    if carry == 1:
+        p.next = ListNode(1)
+    return result.next
+
+
 lk = create_linked_list_head([1, 2, 3, 4])  # 4 > 3 > 2 > 1
 lk_2 = create_linked_list_tail([1, 2, 3, 4])  # 1 > 2 > 3 > 4
 
 print_linked_list(lk)
 print_linked_list(lk_2)
 
+# 通过链表计算 9999+123456=133455
+l1_ = create_linked_list_head([9, 9, 9, 9])
+l2_ = create_linked_list_head([1, 2, 3, 4, 5, 6])
 
+print_linked_list_string(l1_)  # [9, 9, 9, 9]
+print_linked_list_string(l2_)  # [1, 2, 3, 4, 5, 6]
 
+ss = addTwoNumbers(l1_, l2_)
+print_linked_list_string(ss)  # [1, 3, 3, 4, 5, 5]
 
