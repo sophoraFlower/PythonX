640e56155ec01aeb91caf79c0f6c464e1e5aa753
Houle <houle.cf@foxmail.com>
2022-04-05 22:41:03 +0800

    LeetCode 2022/04/05

--- ProgramBasic/leetCode/find_median_sorted_arrays.py
+++ ProgramBasic/leetCode/find_median_sorted_arrays.py
@@ -3,14 +3,50 @@ from typing import List
 
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        temp = []
-        if nums1[-1] <= nums2[0] | nums1[0] >= nums2[-1]:
-            temp = nums1 + nums2
-            temp_len = len(temp)
-            if temp_len % 2 == 0:
-                return (temp[temp_len/2] + temp[(temp_len+2)/2]) /2
-            else:
-                return temp[(temp_len+1)/2]
+        def getKthElement(k):
+            """
+            - 主要思路：要找到第 k (k>1) 小的元素，那么就取 pivot1 = nums1[k/2-1] 和 pivot2 = nums2[k/2-1] 进行比较
+            - 这里的 "/" 表示整除
+            - nums1 中小于等于 pivot1 的元素有 nums1[0 .. k/2-2] 共计 k/2-1 个
+            - nums2 中小于等于 pivot2 的元素有 nums2[0 .. k/2-2] 共计 k/2-1 个
+            - 取 pivot = min(pivot1, pivot2)，两个数组中小于等于 pivot 的元素共计不会超过 (k/2-1) + (k/2-1) <= k-2 个
+            - 这样 pivot 本身最大也只能是第 k-1 小的元素
+            - 如果 pivot = pivot1，那么 nums1[0 .. k/2-1] 都不可能是第 k 小的元素。把这些元素全部 "删除"，剩下的作为新的 nums1 数组
+            - 如果 pivot = pivot2，那么 nums2[0 .. k/2-1] 都不可能是第 k 小的元素。把这些元素全部 "删除"，剩下的作为新的 nums2 数组
+            - 由于"删除" 了一些元素（这些元素都比第 k 小的元素要小），因此需要修改 k 的值，减去删除的数的个数
+            """
+
+            index1, index2 = 0, 0
+            while True:
+                # 特殊情况
+                if index1 == m:
+                    return nums2[index2 + k - 1]
+                if index2 == n:
+                    return nums1[index1 + k - 1]
+                if k == 1:
+                    return min(nums1[index1], nums2[index2])
+
+                # 正常情况
+                new_index1 = min(index1 + k // 2 - 1, m - 1)
+                new_index2 = min(index2 + k // 2 - 1, n - 1)
+                pivot1, pivot2 = nums1[new_index1], nums2[new_index2]
+                if pivot1 <= pivot2:
+                    k -= new_index1 - index1 + 1
+                    index1 = new_index1 + 1
+                else:
+                    k -= new_index2 - index2 + 1
+                    index2 = new_index2 + 1
+
+        m, n = len(nums1), len(nums2)
+        total_length = m + n
+        if total_length % 2 == 1:
+            return getKthElement((total_length + 1) // 2)
         else:
-            insert_index =
+            return (getKthElement(total_length // 2) + getKthElement(total_length // 2 + 1)) / 2
+
+
+test = Solution()
+print(test.findMedianSortedArrays([1, 3, 4, 9], [1, 2, 3, 4, 5, 6, 7, 8, 9]))
+
+list1 = [1, 2, 3, 4]
 
